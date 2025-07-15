23ff0444b637f0f10741d34e9bc110d97cbb00f7
Yonas Behailu <yonasbehailu4@gmail.com>
2025-07-15 08:30:47 +0300

    init python

--- 0x00-python_if_else/21-python_more_on_defining_function.py
+++ 0x00-python_if_else/21-python_more_on_defining_function.py
@@ -1,3 +1,11 @@
 def f(a, L=[]):
     L.append(a)
     return L
+
+# you can write the function like this if you don't want to share between subsequent calls
+
+def f(a, L=None):
+    if L is None:
+        L=[]
+    L.append(a)
+    return L
---
+++ 0x00-python_if_else/23-python_keyword_arguments.py
@@ -0,0 +1,6 @@
+def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
+    print("__ this parrot wouldn't", action, end=' ')
+    print("if you put",voltage,"volts through it.")
+    print("__lovely plumage the",type)
+    print("__ it's",state,"!")
+    
